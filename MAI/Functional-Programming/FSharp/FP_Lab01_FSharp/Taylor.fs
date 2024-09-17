9e48da33f41b70544ba4b13a0f891fb5159b0187
Ivan <ivanboitsov386@gmail.com>
2024-09-17 14:06:38 +0300

    Первая лабораторная работа по F#

---
+++ MAI/Functional-Programming/FSharp/FP_Lab01_FSharp/Taylor.fs
@@ -0,0 +1,55 @@
+﻿module Taylor
+
+// Заданные изначально значения
+let a = 0.0
+let b = 0.5
+let n = 10
+
+
+// Функция вычисления с помощью встроенных функций F#
+let f x : float = float(System.Math.Atan x)
+
+// Наивный способ вычисления ряда Тейлора, где каждый член вычисляется по формуле
+let rec taylorDumb (x: float) n (acc: float) =
+    let epc = 0.00001
+
+    if abs (f x - acc) < epc then
+        acc, n
+    else
+        let newAcc = acc + ((-1.) ** float(n)) * (x ** (2. * float n + 1.)) / (2. * float n + 1.)
+        taylorDumb x (n + 1) newAcc
+
+// Умный способ вычисления ряда Тейлора, где каждый член вычисляется через предыдущий
+let rec taylorSmart (x: float) n (prev: float) (acc: float) =
+    // Определение эпсилона в зависимости от величины x
+    let epc = 
+        if abs x < 0.1 then 0.000001
+        elif abs x < 0.2 then 0.0001
+        elif abs x < 0.3 then 0.001
+        else 0.01
+
+    if abs (f x - acc) <= epc then
+        acc, n
+    else
+        let newPrev = -prev * (x ** 2.0) / float (2 * n + 1)
+        let newAcc = acc + newPrev
+        taylorSmart x (n + 1) newPrev newAcc
+
+let main = 
+    // Красивый вывод таблички
+    printfn "|--------------------------------------------------------------------|"
+    printfn "| %5s | %10s | %10s | %5s |  %10s | %5s |" "x" "Builtin" "Smart Taylor" "#terms" "Dumb Taylor" "#terms"
+    printfn "|--------------------------------------------------------------------|"
+    for i = 0 to n do
+        // Вычисление точки x
+        let x = a + (float i) / (float n) * (b - a)
+        // Вычисление функции наивным методом
+        let dumbResult, _ =  taylorDumb x 0 0.
+        // Вычисление функции умным методом
+        let smartResult, _ =  taylorSmart x 1 x x
+        // Вывод 
+        printfn "| %5.2f | %10.6f | %12.6f | %6d | %12.6f | %6d |" x  (f x) smartResult i dumbResult i
+        printfn "|--------------------------------------------------------------------|"
+
+// Запуск программы
+main
---
+++ MAI/Functional-Programming/FSharp/FP_Lab01_FSharp/equations.fs
@@ -0,0 +1,60 @@
+﻿module equations
+
+// Метод деления пополам (дихотомия)
+let rec dichotomy f (a:float) (b:float) =
+    let eps = 0.000001
+    let xn = (a + b) / 2.
+    let fa = f a
+    let fb = f b
+
+    if abs(f xn) < eps then
+        xn
+    elif fa < fb then
+        if (f xn) < 0. then dichotomy f xn b else dichotomy f a xn
+    else
+        if (f xn) > 0. then dichotomy f xn b else dichotomy f a xn
+
+// Метод итерации
+let rec iterations phi x0 = 
+    let eps = 0.000001
+
+    if abs(x0 - (phi x0)) < eps then
+        x0
+    else
+        let next = phi x0
+        iterations phi next
+
+// Метод Ньютона
+let rec newthon f f' x0 = 
+    let phi x: float = x - (f x) / (f' x)
+    // Используя метод итерации
+    iterations phi x0
+
+// Оригинальные уравнения
+let f1 x: float = 3. * x - 4. * System.Math.Log x - 5. // #23
+
+let f2 x: float = 
+    System.Math.Cos (2./x) - 2. * System.Math.Sin (1./x) + (1./x) // #24
+
+let f3 x: float = System.Math.Sqrt (1. - 0.4 * (x ** 2.))  - System.Math.Asin x // #25
+
+// Первая производная
+let f1' x: float = 3. - 4./x // #23
+
+let f2' x: float = ((2. * System.Math.Cos(1./x) + 2. * System.Math.Sin(2./x))/(x**2.)) - (1./(x**2)) // #24
+
+let f3' x: float = (-2.*x)/(5. * System.Math.Sqrt(1. - ((2.*(x**2.))/5.))) - (1. / (System.Math.Sqrt(1. - (x ** 2.)))) // #25
+
+// Выражение x из первоначального уравнения
+let phi1 x : float =  (4. * System.Math.Log x + 5.) / 3. // #23
+
+let phi2 x : float = 2./(System.Math.Acos(System.Math.Sin(1./x)*2. - 1./x)) // #24
+
+let phi3 x : float = System.Math.Sin(System.Math.Sqrt(1. - 0.4 * (x ** 2.))) // #25
+
+let main = 
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 2. 4.) (iterations phi1 3.) (newthon f1 f1' 3.)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 1. 2.) (iterations phi2 1.5) (newthon f2 f2' 1.5)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.5) (newthon f3 f3' 0.5)
+
+main
